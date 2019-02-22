46ad4ee9452a115c8ea631282c12475f71446529
Reinhard Liess <reinhard.liess@gmail.com>
2019-02-22 17:55:01 +0100

    implement basic data retrieval from Api

--- js/AppPublicApi.js
+++ js/AppPublicApi.js
@@ -7,6 +7,54 @@
 ******************************************/
 
 class AppPublicApi {
+  constructor() {
+    this.addSearch();
+    this.modal = new ModalWindow(this);
+    this.fetchError = false;
+    const api = new RandomUserApi({results: 12, nat: 'US', inc: 'name,location,email,dob,cell,picture'},
+                                  this.onFetchOk, this.onFetchError);
+    this.employees = api.fetch();
+    this.addEmployeesToPage();
+  }
+  /**
+   * Adds employees to page
+   */
+  addEmployeesToPage() {
+    if (this.fetchError) {
+      return
+    }
 
+  }
+
+  /**
+   * Callback in case of a a succesful fetch operation
+   * @param {object} data from Api
+   */
+  onFetchOk(data) {
+    // console.log({data});
+
+  }
+
+  /**
+   * Callback in case of a fetch error
+   * @param {error} error object
+   */
+  onFetchError(error) {
+    this.fetchError = true;
+
+  }
+
+  /**
+   * Add a search input field to the website
+   */
+  addSearch() {
+    const divSearch = document.querySelector('.search-container');
+    const html =
+      `<form action="#" method="get">
+          <input type="search" id="search-input" class="search-input" placeholder="Search...">
+          <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+       </form>`;
+    divSearch.insertAdjacentHTML('afterbegin', html);
+  }
 }
 
--- js/RandomUserApi.js
+++ js/RandomUserApi.js
@@ -5,7 +5,53 @@
   FSJS project 5 - Public API Request
   Reinhard Liess, 2019
 ******************************************/
-
+// class for access to the Random User Api: https://randomuser.me/
 class RandomUserApi {
+  /**
+   * Initialize Api
+   * @param {object} Api options as key/value pairs
+   * @param {function} Function called in case of successful fetch operation
+   * @param {function} [callback] Function called in case of error
+   */
+  constructor(objParams, cbOnFetchOk, cbOnError) {
+    this.urlApi = 'https://randomuser.me/api/' + this.buildUri(objParams);
+    this.cbOnFetchOk = cbOnFetchOk;
+    this.cbOnError = cbOnError;
+    this.employees = null;
+  }
 
+  /**
+   * Builds URI from object
+   * @param {object} Api options as key/value pairs
+   * @return {string} URI encoded string
+   */
+  buildUri(objParams) {
+    let strOptions = '?';
+    for (const prop in objParams) {
+      if (objParams.hasOwnProperty(prop)) {
+        strOptions += (`${prop}=${objParams[prop]}&`);
+      }
+    }
+    return strOptions.slice(0, -1);
+  }
+
+  /**
+   * Retrieves API data
+   */
+  fetch() {
+    fetch(this.urlApi)
+      // .then(this.checkStatus)
+      .then(response => response.json())
+      .then(data => this.cbOnFetchOk(data))
+      .catch(error => console.error('Error accessing the API, specifically: ', error));
+  }
+
+  checkStatus(response) {
+    console.log({response});
+    if (response.ok) {
+      return Promise.resolve(response);
+    } else {
+      return Promise.reject(new Error(response.statusText));
+    }
+  }
 }
